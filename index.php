5fa2930773ecd6eaa9e647b4ad7448a0b2dce99e
endyendy <41787965+endyendy@users.noreply.github.com>
2020-05-03 01:57:52 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
 <?php echo "Hello world"; ?>
+$mysqli = new mysqli(getenv("HELLO_PHP_MYSQL_SERVICE_HOST"), getenv("MYSQL_USER"), getenv("MYSQL_PASSWORD"), getenv("MYSQL_DATABASE"));
+if ($mysqli->connect_error) {
+    exit($mysqli->connect_error);
+} else {
+    $mysqli->set_charset("utf8");
+}
+$result = $mysqli->query("select 1") or exit($mysqli->error());
+echo var_dump($result->fetch_assoc());
+$mysqli->close();
